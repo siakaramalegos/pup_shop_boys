913b65466081671de88da3e01a3d617bb9b44ddc
Sia <siakaramalegos@gmail.com>
2015-08-27 11:05:48 -0500

    added carts and line items

---
+++ app/models/line_item.rb
@@ -0,0 +1,26 @@
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+
+  def total_price
+    price * quantity
+  end
+end
+
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  cart_id    :integer
+#  quantity   :integer          default(1)
+#  price      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_line_items_on_cart_id     (cart_id)
+#  index_line_items_on_product_id  (product_id)
+#
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :brand
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :name, :description, :category_id, :brand_id, :price, :quantity, presence: true
   validates :name, uniqueness: true
@@ -8,6 +10,17 @@ class Product < ActiveRecord::Base
 
   has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: ":style_missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  private
+    # ensure there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
 
 # == Schema Information
