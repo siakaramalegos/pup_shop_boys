d54e6739182e054c1713b0bd1b66e38cb8f5940c
Sia <siakaramalegos@gmail.com>
2015-08-31 10:22:53 -0500

    Added order validation, set user for an order, associated line items with an order on checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,19 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :line_items, dependent: :destroy
 
+  validates :name, :address, :user_id, presence: true
+
   PAYMENT_TYPES = ["Check", "Credit Card", "Paypal"]
+  validates :payment_type, inclusion: PAYMENT_TYPES
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      # this line sets the order id for each line item because
+      # we are prepending this with @order. from the controller
+      line_items << item
+    end
+  end
 end
 
 # == Schema Information
