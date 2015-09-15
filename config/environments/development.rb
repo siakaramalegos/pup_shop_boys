10f2db093186a4181c2257aa9e369de3effbe4c8
Sia <siakaramalegos@gmail.com>
2015-09-15 09:51:11 -0500

    Set up gmail for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,19 @@ Rails.application.configure do
   # Use letter opener to deliver emails in development
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: Rails.application.secrets.domain_name,
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: Rails.application.secrets.email_provider_username,
+  #   password: Rails.application.secrets.email_provider_password
+  # }
+
+  # config.action_mailer.default_url_options = {:host => Rails.application.secrets.domain_name}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
